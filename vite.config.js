fd696fff803bdc9eea6c470042c1e956c5271ebd
tejasfirange <tj0211f@gmail.com>
2025-10-17 04:37:22 +0530

    custom domain

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/webapp-naruto/', // <-- Replace with your actual GitHub repo name
+  base: '/', // <-- Replace with your actual GitHub repo name
 
 })
