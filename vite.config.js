f232c87ee4a8144ebb8a2f452f39273252f4d062
tejasfirange <tj0211f@gmail.com>
2025-10-17 11:28:04 +0530

    Remove project files and configuration for my-react-app

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/', // <-- Replace with your actual GitHub repo name
-
-})
