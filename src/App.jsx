384e6df68fab55ffe0b04903b326c0adffb11cd4
tejasfirange <tj0211f@gmail.com>
2025-10-17 12:21:09 +0530

    App changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import NarutoMapApp from "./pages/LandingPageMap";
 
@@ -6,8 +6,8 @@ function App() {
   return (
     <Router>
       <nav style={{ display: "flex", gap: "20px", padding: "10px", background: "#1b2a36" }}>
-        <Link to="/" style={{ color: "#fff", textDecoration: "none" }}>🏠 Home</Link>
-        <Link to="/map" style={{ color: "#fff", textDecoration: "none" }}>🗺️ Naruto Map</Link>
+        <Link to="/" style={{ color: "#fff" }}>🏠 Home</Link>
+        <Link to="/map" style={{ color: "#fff" }}>🗺️ Naruto Map</Link>
       </nav>
 
       <Routes>
